44e2295badca68fb36127d535705b5eb41a75d1d
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2022-03-10 00:35:43 -0300

    01. Conhecendo o ambiente
    Signed-off-by: Camila Cavalcante <camiladsantoscavalcante@gmail.com>

---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss']
+})
+export class NovaTransferenciaComponent {
+
+}
