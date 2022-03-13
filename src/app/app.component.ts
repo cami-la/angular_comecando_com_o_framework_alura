4535d003c9a3389108cda65e02b9027e9cfa22d0
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2022-03-13 12:08:19 -0300

    04. Transferencias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bytebank';
-  transferencia: any;
+  transferencias: any[] = [];
 
   transferir($event){
     console.log($event);
-    this.transferencia = $event;
+    const transferencia = {...$event, data: new Date()};
+    this.transferencias.push(transferencia);
   }
 }
---
+++ src/app/extrato/extrato.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-extrato',
+  templateUrl: './extrato.component.html',
+  styleUrls: ['./extrato.component.scss']
+})
+export class ExtratoComponent implements OnInit {
+  @Input() transferencias: any[];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
