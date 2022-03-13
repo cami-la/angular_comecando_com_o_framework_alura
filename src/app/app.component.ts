ee818c3f86bad4874b55d93de731f636e3921aae
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2022-03-12 23:02:45 -0300

    03. Exportando dados do componente
    Signed-off-by: Camila Cavalcante <camiladsantoscavalcante@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bytebank';
+  transferencia: any;
+
+  transferir($event){
+    console.log($event);
+    this.transferencia = $event;
+  }
 }
