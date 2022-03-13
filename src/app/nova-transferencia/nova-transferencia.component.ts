9f51a71074b09f5d3ac710812519b6735cabf362
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2022-03-12 23:02:45 -0300

    03. Exportando dados do componente
    Signed-off-by: Camila Cavalcante <camiladsantoscavalcante@gmail.com>

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Output, EventEmitter } from "@angular/core";
 
 @Component({
   selector: 'app-nova-transferencia',
@@ -6,11 +6,24 @@ import { Component } from "@angular/core";
   styleUrls: ['./nova-transferencia.component.scss']
 })
 export class NovaTransferenciaComponent {
+
+  @Output() aoTransferir = new EventEmitter<any>();
+
   valor: number;
   destino: number;
 
   public transferir(): void {
     console.log(`Solicitada uma nova transferência valor = ${this.valor} destino = ${this.destino}`);
+    const valorEmitir = {
+      valor: this.valor,
+      destino: this.destino
+    };
+    this.aoTransferir.emit(valorEmitir);
+    this.limparFormulario();
+  }
+  private limparFormulario(): void {
+    this.valor = 0;
+    this.destino = 0;
   }
 }
 
