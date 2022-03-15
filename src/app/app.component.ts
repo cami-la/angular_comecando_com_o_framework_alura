16b27db59c20b5fe62217f5ca2228b0e3f9c27d1
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2022-03-15 05:17:45 -0300

    05. Service
    Signed-off-by: Camila Cavalcante <camiladsantoscavalcante@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TransferenciaService } from './services/transferencia.service';
 
 @Component({
   selector: 'app-root',
@@ -7,11 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bytebank';
-  transferencias: any[] = [];
+
+  constructor(private service: TransferenciaService){
+
+  }
 
   transferir($event){
+    this.service.adicionar($event);
     console.log($event);
-    const transferencia = {...$event, data: new Date()};
-    this.transferencias.push(transferencia);
   }
 }
